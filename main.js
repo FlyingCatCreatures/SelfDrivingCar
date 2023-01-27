4e298d6e53dccee478ff338f5d3041d2e1cf10b5
Fabmans06 <fabiangroen@live.nl>
2023-01-27 21:04:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ const trafficCount = 200
 const trafficDistance = 205
 const ctx=canvas.getContext("2d");
 const road=new Road(canvas.width/2,canvas.width*0.9);
-const car=new Car(road.getLaneCenter(Math.floor(road.laneCount/2)),300,60,120,"KEYS",maxSpeed);
+const car=new Car(road.getLaneCenter(Math.floor(road.laneCount/2)),300,78,132,"KEYS",maxSpeed);
 const traffic = [
     //new Car(road.getLaneCenter(Math.floor(Math.random()*(road.laneCount))),(Math.floor(Math.random()*(-101))*100),30,60,"STATIONARYDUMMY"),
 ]
 for (let i=0;i<trafficCount;i++){
-    traffic.push(new Car(road.getLaneCenter(Math.floor(Math.random()*(road.laneCount))),(Math.floor(Math.random()*(-trafficDistance))*100),60,120,"MOVINGDUMMY",Math.floor(Math.random()*(maxSpeed+1))+1))
+    traffic.push(new Car(road.getLaneCenter(Math.floor(Math.random()*(road.laneCount))),(Math.floor(Math.random()*(-trafficDistance))*100),78,132,"MOVINGDUMMY",Math.floor(Math.random()*(maxSpeed+1))+1))
 }
 //console.log(traffic)
 const trafficimg = new Image();  

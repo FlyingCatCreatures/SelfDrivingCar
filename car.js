2d6af1877858a50be41988990d7f23147d6e176d
FlyingCatCreatures <harstaanne@gmail.com>
2023-01-27 21:02:25 +0100

    Update car.js

--- car.js
+++ car.js
@@ -15,6 +15,8 @@ class Car{
         this.controlType=controlType
         this.drawStyle=drawStyle
 
+        this.useBrain=controlType=="AI";
+
         if(this.controlType=="KEYS"){
             this.sensor=new Sensor(this);
             this.brain=new NeuralNetwork(
@@ -37,7 +39,15 @@ class Car{
                 s=>s==null?0:1-s.offset
             );
             const outputs=NeuralNetwork.feedForward(offsets,this.brain);
-            console.log(outputs);
+            //console.log(outputs);
+
+            if(this.useBrain){
+                this.controls.forward=outputs[0];
+                this.controls.left=outputs[1];
+                this.controls.right=outputs[2];
+                this.controls.reverse=outputs[3];
+
+            }
         }
         //console.table(this.speed,this.y,this.x, this.angle)
         }  

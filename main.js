22be38161bd223b715d951f040ad1b7b696067a1
Fabmans06 <fabiangroen@live.nl>
2023-01-28 19:32:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -109,8 +109,8 @@ function render(){
     }
     bestCar.draw(carCtx, carimg ,traffic, true)
     carCtx.restore();
-    //requestAnimationFrame(render);
-    setTimeout(render, 1000/30)
+    requestAnimationFrame(render);
+    //setTimeout(render, 1000/30)
 }
 
 function generateCars(Amount){
